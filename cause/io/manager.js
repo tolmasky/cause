58ea0cb8464968e57ad600ed5d305ccd23b70cd6
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-02-06 13:00:22 -0800

    Hack to avoid climbing too far searching for IOs.
    Reviewed by @tolmasky.

--- cause/io/manager.js
+++ cause/io/manager.js
@@ -102,6 +102,7 @@ function canContainIOs(node)
         return false;
 
     return  node instanceof Record ||
+            // Hack until we use real types to determine this.
             ((node["@@__IMMUTABLE_KEYED__@@"] ||
             node["@@__IMMUTABLE_INDEXED__@@"]) && node.size > 0 && canContainIOs(node.first()));
 }
