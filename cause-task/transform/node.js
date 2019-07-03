41aa9d776c51a5a0138d105d2fd1bd5cd0cbe8e1
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-03 14:12:59 -0700

    Better type checking for algebraic nodes.
    Reviewed by @tolmasky.

--- cause-task/transform/node.js
+++ cause-task/transform/node.js
@@ -4,18 +4,28 @@ const fail = require("@algebraic/type/fail");
 
 const t = require("@babel/types");
 const fromDefinition = ({ optional, validate }) =>
-   (type => optional ?
-        [Optional(type), None] :
-        type)(fromValidate(validate) || Object);
+   (deferred => optional ?
+        () => [Optional(deferred()), None] :
+        deferred)(fromValidate(validate) || (() => Object));
+const cached = f => (cache => (...args) =>
+    cache[JSON.stringify(args)] ||
+    (cache[JSON.stringify(args)] = f(...args)))
+    (Object.create(null));
+const fromOneOfNodeTypes = cached(names =>
+    union ([`${JSON.stringify(names)}`])
+        (...names.map(name =>
+            toDeferredField(name, () => concrete[name] || aliases[name]))));
 const fromValidate = validate =>
-    !validate ? Object :
+    !validate ? (() => Object) :
     validate.type && validate.type !== "array" ?
-        primitives[validate.type] :
+        () => primitives[validate.type] :
+    validate.oneOfNodeTypes ?
+        () => fromOneOfNodeTypes(validate.oneOfNodeTypes) :
     validate.chainOf ?
         validate.chainOf.map(fromValidate).find(x => !!x) :
     false;
-const toField = (name, type) =>
-    (new Function(`return ${name === "extends" || name === "default" || name === "const" ? "_extends" : name } => arguments[0]`))(type);
+const toDeferredField = (name, deferred) =>(console.log((new Function(`return ${name === "extends" || name === "default" || name === "const" ? "_extends" : name } => arguments[0]()`))(deferred)+""),
+    (new Function(`return ${name === "extends" || name === "default" || name === "const" ? "_extends" : name } => arguments[0]()`))(deferred));
 
 const concrete = Object.fromEntries(t
     .TYPES
@@ -23,9 +33,10 @@ const concrete = Object.fromEntries(t
     .map(name => [name, data ([name]) (...Object
         .entries(t.NODE_FIELDS[name])
         .map(([name, definition]) => [name, fromDefinition(definition)])
-        .map(([name, type]) => toField(name, type)))]));
-
-
-
-console.log(concrete.RestElement({ argument: concrete.Identifier({ name:"hi" }) }));
-
+        .map(([name, type]) => toDeferredField(name, type)))]));
+const aliases = Object.fromEntries(Object
+    .entries(t.FLIPPED_ALIAS_KEYS)
+    .map(([name, aliases]) =>
+        [name, union ([name]) (...aliases.map(name => concrete[name]))]));
+const c = concrete;
+console.log(c.FunctionExpression({ id: c.Identifier({ name: "name" }), params:[], body:c.BlockStatement([]) }));
