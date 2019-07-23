3a4d437731b7257c0f23f46bbc421fcf4cccfc05
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-23 09:17:50 -0700

    Remove unused files.
    Reviewed by @tolmasky.

--- cause-task/transform/unfold.js
+++
@@ -1,7 +0,0 @@
-const push = (item, array) => (array.push(item), array);
-const unfold = (f, v, into = []) =>
-    (result => result === false ?
-        into : push(result[0], unfold(f, result[1], into)))
-    (f(v));
-
-module.exports = unfold;
